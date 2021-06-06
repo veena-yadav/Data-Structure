b4f66667288ad9de1921c235ee37d513bff95f46
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-06-06 19:18:43 +0530

    q2

--- MaxMinOfArray.py
+++ MaxMinOfArray.py
@@ -1,19 +1,24 @@
 # #Maximum and minimum of an array using minimum number of comparisons
 
-arr= [1,5,8,6,10]
-def maxMin(arr):
-    max = arr[0]
-    min = arr[0]
+arr= [1000, 11, 445, 1, 330, 3000]
+def max(arr):
+    max= arr[0]
     for i in range(len(arr)):
         if max <= arr[i]:
             max= arr[i]
-    
-        if min >= i:
-            min= arr[i]
-    print('max',max)
-    print('min',min)
+    return max
 
-maxMin(arr)
+def max(arr):
+    min= arr[0]
+    for i in range(len(arr)):
+        if min >= i:
+            min= arr[i]  
+    return min
+
+def maxMin(arr):
+    return (max(arr),min(arr))
+
+print(maxMin(arr))
 
 # To calculate max
 # arr= [1000, 11, 445, 1, 330, 3000]
@@ -37,3 +42,42 @@ maxMin(arr)
 #     return min
 
 # print('minimun number: ' , min(arr))
+
+
+# def getMinMax(low, high, arr):
+# 	arr_max = arr[low]
+# 	arr_min = arr[low]
+	
+# 	# If there is only one element
+# 	if low == high:
+# 		arr_max = arr[low]
+# 		arr_min = arr[low]
+# 		return (arr_max, arr_min)
+		
+# 	# If there is only two element
+# 	elif high == low + 1:
+# 		if arr[low] > arr[high]:
+# 			arr_max = arr[low]
+# 			arr_min = arr[high]
+# 		else:
+# 			arr_max = arr[high]
+# 			arr_min = arr[low]
+# 		return (arr_max, arr_min)
+# 	else:
+		
+		
+# 		mid = int((low + high) / 2)
+# 		arr_max1, arr_min1 = getMinMax(low, mid, arr)
+# 		arr_max2, arr_min2 = getMinMax(mid + 1, high, arr)
+
+# 	return (max(arr_max1, arr_max2), min(arr_min1, arr_min2))
+
+# # Driver code
+# arr = [1000, 11, 445, 1, 330, 3000]
+# high = len(arr) - 1
+# low = 0
+# arr_max, arr_min = getMinMax(low, high, arr)
+# print('Minimum element is ', arr_min)
+# print('nMaximum element is ', arr_max)
+
+
