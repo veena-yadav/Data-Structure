cc21c02686fccc07fd3519a869a1e1f4c995d963
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-06-08 17:10:54 +0530

    doubly linked list

--- bsic.py
+++ bsic.py
@@ -61,77 +61,181 @@
 
 # linked list
 
-class Node:
-    def __init__(self,data = None, next = None):
-        self.data = data
-        self.next = next
+# class Node:
+#     def __init__(self,data = None, next = None):
+#         self.data = data
+#         self.next = next
 
 
-class LL:
-    def __init__(self):
-        self.head = head
+# class LL:
+#     def __init__(self):
+#         self.head = None
 
-# printing a data
-    def printList(self):
-        temp = self.head
-        while(temp):
-            print(temp.data)
-            temp = temp.next
+# # printing a data
+#     def printList(self):
+#         temp = self.head
+#         while(temp):
+#             print(temp.data)
+#             temp = temp.next
 
-# push a element in front
-    def pushBeg(self,newData):
-        newNode = Node(newData)
-        newNode.next = self.head
-        self.head = newNode
+# # push a element in front
+#     def pushBeg(self,newData):
+#         newNode = Node(newData)
+#         newNode.next = self.head
+#         self.head = newNode
 
-# push a element at given point
-    def pushAfter(self, prevNode ,newData):
+# # push a element at given point
+#     def pushAfter(self, prevNode ,newData):
     
-        if prevNode is None:
-            print('list is empty')
+#         if prevNode is None:
+#             print('list is empty')
         
-        newNode = Node(newData)
-        newNode.next = prevNode.next
-        prevNode.next = newNode
+#         newNode = Node(newData)
+#         newNode.next = prevNode.next
+#         prevNode.next = newNode
 
-# push a element at end point
-    def pushLast(self, newData):
-        newNode = Node(newData)
+# # push a element at end point
+#     def pushLast(self, newData):
+#         newNode = Node(newData)
+#         if self.head is None:
+#             self.head = newNode
+#             return
+        
+#         last = self.head
+#         while (last.next):
+#             last = last.next
+#             last.next = newNode
+
+# # Deleting a node
+
+#     def DeleteAfter(self, pos):
+#         curNode = self.head
+#         prev = None
+#         count = 0
+
+#         while curNode and count != pos:
+#             prev = curNode
+#             curNode = curNode.next
+#             count += 1
+
+#         if curNode is None:
+#             return
+
+#         prev.next = curNode.next
+#         curNode = None
+        
+
+# linkedlist = LL()
+# linkedlist.pushBeg(5)
+# linkedlist.pushBeg(10)
+# # linkedlist.pushAfter(1,15)
+# linkedlist.pushLast(20)
+# linkedlist.printList()
+# linkedlist.DeleteAfter(1)
+
+#------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoubltLinkedlist:
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        cur = self.head
+        while cur:
+            print(cur.data)
+            cur = cur.next
+
+    def append(self,data):
         if self.head is None:
+            newNode = Node(data)
+            newNode.prev = None
             self.head = newNode
-            return
-        
-        last = self.head
-        while (last.next):
-            last = last.next
-            last.next = newNode
+        else:
+            newNode = Node(data)
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next= newNode
+            newNode.prev = cur
+            newNode.next = None
 
-# Deleting a node
+    def prepand(self,data):
+        if self.head is None:
+            newNode = Node(data)
+            newNode.prev = None
+            self.head = newNode
 
-    def DeleteAfter(self, pos):
-        curNode = self.head
-        prev = None
-        count = 0
+        else:
+            newNode = Node(data)
+            self.head.prev = newNode
+            newNode.next = self.head
+            self.head = newNode
+            newNode.prev = None
 
-        while curNode and count != pos:
-            prev = curNode
-            curNode = curNode.next
-            count += 1
+    def delete(self,key):
+        cur = self.head
 
-        if curNode is None:
-            return
+        while cur:
+            if cur.data== key and cur ==self.head:
+                #if only one node is there
+                if not cur.next:
+                    cur = None
+                    self.head = None
+                    return
 
-        prev.next = curNode.next
-        curNode = None
-        
+                # to delete first node
+                else:
+                    nxt = cur.next
+                    cur.next = None
+                    nxt.prev = None
+                    cur = None
+                    self.head = nxt
+                    return
 
-linkedlist = LL()
-linkedlist.pushBeg(5)
-linkedlist.pushBeg(10)
-linkedlist.pushAfter(1, 15)
-linkedlist.pushLast(20)
-linkedlist.printList()
-linkedlist.DeleteAfter(1)
+            elif cur.data == key:
+                # To delete middle element
+                if cur.next:
+                    nxt = cur.next
+                    prev = cur.prev
+                    prev.next = nxt
+
+                    nxt.prev = prev
+                    cur.next = None
+                    cur.prev = None
+                    cur = None
+                    return
+
+                else:
+                    prev = cur.prev
+                    prev.next = None
+                    cur.prev = None
+                    cur = None
+                    return
+            cur = cur.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
