5385c9f1a55f32cb2db40049f6a123b8a957d19f
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-06-13 15:55:06 +0530

    sorting negative element asside

---
+++ negtivesideInArray.py
@@ -0,0 +1,25 @@
+# Move all negative numbers to beginning and positive to end with constant extra space
+
+def NegativeSideInArray(arr, n):
+    cnt = 0
+    cnt1 = 0
+    l = []
+    for i in range(0,n):
+        if arr[i] < 0:
+            l.append(arr[i])
+        else:
+            cnt1 += 1
+            
+            
+    for i in range(n):
+        if arr[i] > 0:
+            l.append(arr[i])
+            
+    return l
+
+arr = [-12, 11, -13, -5, 6, -7, 5, -3, -6]
+# arr = [-1, 2, -3, 4, 5, 6, -7, 8, 9]
+n = len(arr)
+ls = NegativeSideInArray(arr,n)
+print(ls)
+        
