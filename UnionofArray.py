bd3d9a61be084ce62e1af0c980a7e313e5405b50
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-06-18 19:49:49 +0530

    done

---
+++ UnionofArray.py
@@ -0,0 +1,46 @@
+# Union  of array
+
+a1 = [1, 2, 3, 4, 5]
+a2 = [1, 2, 3]
+m = len(a1)
+n = len(a2)
+# a1.extend(a2)
+# ls = set(a1)
+
+# cnt = len(ls)
+# print(cnt)
+
+# # Intersection with arrary
+
+# for i in a2:
+#     if i not in a1:
+#         a1.remove(i)
+        
+# print(a1)
+
+def arr(a1,a2, m,n):
+    i=0
+    j=0
+    while i<m and j<n:
+        if a1[i]<a2[j]:
+            print(a1[i])
+            i+= 1
+            
+        elif a2[j]<a2[i]:
+            print(a2[j])
+            j += 1
+            
+        else:
+            print(a2[j])
+            i += 1
+            j += 1
+            
+    while i< m:
+        print(a1[i])
+        i += 1
+        
+    while j<n:
+        print(a2[i])
+        j += 1
+        
+arr(a1, a2, m, n)
