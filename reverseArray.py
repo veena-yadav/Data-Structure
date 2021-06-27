f8c5dd1f45280353042dddddf42d3b3a9c008966
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-06-27 23:01:20 +0530

    reversearray

--- reverseArray.py
+++ reverseArray.py
@@ -22,3 +22,16 @@ ReverseArray(arr,0,7)
 # print('Reverse array')
 # reverseList(arr)
 
+# Reverse a array only one step ahead
+def rotate(arr, n):
+    x = arr[n - 1]
+     
+    for i in range(n - 1, 0, -1):
+        arr[i] = arr[i - 1]
+         
+    arr[0] = x
+    return arr
+    
+arr = [1,2,3,4,5]
+n = len(arr)
+print(rotate(arr,n))
