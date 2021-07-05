049e92b82e31dfa23940bcede9e26d4f48e526ce
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-05 13:42:07 +0530

    done

--- array/duplicatesInArray.py
+++ array/duplicatesInArray.py
@@ -6,14 +6,8 @@ def Duplicates(arr):
         
         
 arr = [1,2,3,4,5,5]
-# print(Duplicates(arr))
+print(Duplicates(arr))
 
-# n = 4, m = 5
-# arr1[] = {1, 3, 5, 7}
-# arr2[] = {0, 2, 6, 8, 9}
-
-# def mergeSortedArr(arr1,arr2,n,m):
-#     for i in 
 
 
 # Search a elment in arrary
