a061e9be48e4dff5c12e403b293156aa68856d22
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-05 15:05:23 +0530

    done

--- array/duplicatesInArray.py
+++ array/duplicatesInArray.py
@@ -20,3 +20,17 @@ def SearchArr(arr,n,x):
     return("element does not exit")
     
 print(SearchArr(arr1,n,34))
+
+# count frequency of element in a sorted arrray
+arr1 = [12, 34, 10,12,12, 6, 40]
+n = len(arr)
+def countfreq(arr,n,x):
+    count = 0
+    arr.sort()
+    for i in range(n):
+        if arr[i] == x:
+            count += 1
+    return count
+    # return("element does not exit")
+    
+print(countfreq(arr1,n,12))
