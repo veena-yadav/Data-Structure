934fbeaa234d5090fe873af330a4b2e49d71fb9f
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-13 15:48:41 +0530

    5

--- array/Searching.py
+++ array/Searching.py
@@ -160,3 +160,53 @@ def peakElement(self,arr, n):
         for i in range(1,n-1):
             if (arr[i]>= arr[i-1] and arr[i]>= arr[i+1]):
                 return i
+            
+#########################################################################
+
+# find element in sorted and rotated arrary
+def Search(arr,n,k):
+    #code here
+    pivot = findPivot(arr, 0, n-1)
+ 
+    if pivot == -1:
+        return binarySearch(arr, 0, n-1, k)
+
+    if arr[pivot] == k:
+        return pivot
+    if arr[0] <= k:
+        return binarySearch(arr, 0, pivot-1, k)
+    return binarySearch(arr, pivot + 1, n-1, k)
+ 
+ 
+def findPivot(arr, low, high):
+     
+    # base cases
+    if high < low:
+        return -1
+    if high == low:
+        return low
+     
+    mid = int((low + high)/2)
+     
+    if mid < high and arr[mid] > arr[mid + 1]:
+        return mid
+    if mid > low and arr[mid] < arr[mid - 1]:
+        return (mid-1)
+    if arr[low] >= arr[mid]:
+        return findPivot(arr, low, mid-1)
+    return findPivot(arr, mid + 1, high)
+ 
+
+def binarySearch(arr, low, high, k):
+ 
+    if high < low:
+        return -1
+         
+    mid = int((low + high)/2)
+     
+    if k == arr[mid]:
+        return mid
+    if k > arr[mid]:
+        return binarySearch(arr, (mid + 1), high,k)
+    return binarySearch(arr, low, (mid -1), k)
+ 
