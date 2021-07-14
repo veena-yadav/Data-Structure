21caff90efb3998b1258b681f402f40c058fc45e
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-14 20:50:34 +0530

    1

--- array/Searching.py
+++ array/Searching.py
@@ -265,3 +265,16 @@ def minimumPlatform(self,n,arr,dep):
                 res = plat
             
         return res
+    
+    ########################################################
+"""
+    Algorithm	Time Complexity	 
+ 	            Best	    Average	    Worst	 
+Selection Sort	Ω(n^2)	    θ(n^2)	    O(n^2)	 
+Bubble Sort 	Ω(n)	    θ(n^2)	    O(n^2)	 
+Insertion Sort	Ω(n)	    θ(n^2)	    O(n^2)	 
+Heap Sort	    Ω(n log(n))	θ(n log(n))	O(n log(n))	 
+Quick Sort	    Ω(n log(n))	θ(n log(n))	O(n^2)	 
+Merge Sort	    Ω(n log(n))	θ(n log(n))	O(n log(n))	 
+Bucket Sort	    Ω(n+k)	    θ(n+k)	    O(n^2)	 
+Radix Sort	    Ω(nk)	    θ(nk)	    O(nk)           """
