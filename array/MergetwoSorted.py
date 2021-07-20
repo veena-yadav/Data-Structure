97cde77d4ff1b2a594ccbef2436e501adc288d3c
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-20 22:01:26 +0530

    1

--- array/MergetwoSorted.py
+++ array/MergetwoSorted.py
@@ -564,3 +564,29 @@ def NearToGiven(arr,n,x):
 # arr = [1, 3, 4, 7, 10]
 # n = len(arr)
 # print(NearToGiven(arr,n,15))
+
+####################################################################
+
+def findMinDiff(arr, n, m):
+
+	if (m==0 or n==0):
+		return 0
+
+	arr.sort()
+
+	if (n < m):
+		return -1
+
+	min_diff = arr[n-1] - arr[0]
+
+	for i in range(len(arr) - m + 1):
+		min_diff = min(min_diff , arr[i + m - 1] - arr[i])
+	
+		
+	return min_diff
+
+arr = [12, 4, 7, 9, 2, 23, 25, 41,30, 40, 28, 42, 30, 44, 48,43, 50]
+m = 7
+n = len(arr)
+print("Minimum difference is", findMinDiff(arr, n, m))
+	
