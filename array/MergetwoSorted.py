a2b92f81956c32795358beb1d8a6ae839b1828d5
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-21 20:23:20 +0530

    5

--- array/MergetwoSorted.py
+++ array/MergetwoSorted.py
@@ -639,3 +639,23 @@ def coinsum(s,m,n):
 # n = 4
 # x = coinsum(arr, m, n)
 # print (x)
+######################################################3
+
+def LAS(arr, n):
+	inc = 1
+	dec = 1
+
+	for i in range(1,n):
+	
+		if (arr[i] > arr[i-1]):
+			inc = dec + 1
+   
+		elif (arr[i] < arr[i-1]):
+			dec = inc + 1
+			
+	return max(inc, dec)
+
+
+# arr = [10, 22, 9, 33, 49, 50, 31, 60]
+# n = len(arr)
+# print(LAS(arr, n))
