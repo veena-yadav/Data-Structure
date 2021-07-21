85595019677c9967117e3122c137ef7d23debeb9
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-21 19:44:44 +0530

    2

--- array/MergetwoSorted.py
+++ array/MergetwoSorted.py
@@ -590,3 +590,36 @@ def findMinDiff(arr, n, m):
 # n = len(arr)
 # print("Minimum difference is", findMinDiff(arr, n, m))
 	
+ ###############################################################################
+# Program to print all combination of size r in an array of size n
+
+def printCombination(arr, n, r):
+	data = [0] * r
+	combinationUtil(arr, n, r, 0, data, 0)
+
+''' 
+data[] ---> Temporary array to store current combination
+i	   ---> index of current element in arr[]	 '''
+def combinationUtil(arr, n, r, index, data, i):
+
+	if (index == r):
+		for j in range(r):
+			print(data[j], end = " ")
+		print()
+		return
+
+	if (i >= n):
+		return
+
+	data[index] = arr[i]
+	combinationUtil(arr, n, r, index + 1, data, i + 1)
+
+	combinationUtil(arr, n, r, index, data, i + 1)
+
+arr = [1, 2, 3, 4, 5]
+r = 3
+n = len(arr)
+printCombination(arr, n, r)
+
+
+
