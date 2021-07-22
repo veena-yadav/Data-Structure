96c7b405c31ce8970e35a17a77b2467e259464c3
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-22 16:03:38 +0530

    5

--- array/Matrix.py
+++ array/Matrix.py
@@ -45,3 +45,43 @@ C=A[:][:] # To store result
  
 # add(A, B, C)
 # traversematrix(C,4,4)
+
+#########################################################
+# multiplication of matrix:
+
+def multiplyMatrix(m1, m2, mat1,
+			n1, n2, mat2):
+
+	res = [[0 for x in range(n2)]
+			for y in range (m1)]
+	
+	for i in range(m1):
+		for j in range(n2):
+			res[i][j] = 0
+			for x in range(m2):		
+				res[i][j] += (mat1[ i][x] * mat2[ x][j])
+			
+	for i in range(m1):
+		for j in range(n2):	
+			print (res[i][j],
+				end = " ")	
+		print ()
+    
+
+
+mat1 = [[2, 4], [3, 4]]
+mat2 = [[1, 2], [1, 3]]
+m1, m2, n1, n2 = 2, 2, 2, 2
+
+multiplyMatrix(m1, m2, mat1,n1, n2, mat2)
+	
+
+
+    
+    
+    
+    
+    
+    
+    
+    
