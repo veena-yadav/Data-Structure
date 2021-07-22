7a92d05aa672ea871e188553affa1c6db776bff6
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-22 15:02:43 +0530

    1

---
+++ array/Matrix.py
@@ -0,0 +1,9 @@
+def traversematrix(arr,r,c):
+    for i in range(r):
+        for j in range(c):
+            print(arr[i][j],end = " ")
+        print()
+            
+            
+# arr=[ [1,2,3],[4,5,6],[7,8,9]]
+# traversematrix(arr,3,3)
