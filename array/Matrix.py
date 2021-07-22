7829926853fb722827fb0ea763c8a7697fd83107
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-22 15:12:41 +0530

    2

--- array/Matrix.py
+++ array/Matrix.py
@@ -7,3 +7,17 @@ def traversematrix(arr,r,c):
             
 # arr=[ [1,2,3],[4,5,6],[7,8,9]]
 # traversematrix(arr,3,3)
+
+def searchingInMatrix(arr,r,c,key):
+    flag = False
+    for i in range(r):
+        for j in range(c):
+            if arr[i][j] == key:
+                flag = True
+    
+    if flag == True:
+        return("Element is present")
+    return("No such Element")
+
+# arr=[ [1,2,3],[4,5,6],[7,8,9]]
+# print(searchingInMatrix(arr,3,3,13))
