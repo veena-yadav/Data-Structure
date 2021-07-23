fcda78ee6bdcf4b200d4f6bf8314612c0df00cbb
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-23 23:29:02 +0530

    1

--- array/Matrix.py
+++ array/Matrix.py
@@ -49,8 +49,7 @@ C=A[:][:] # To store result
 #########################################################
 # multiplication of matrix:
 
-def multiplyMatrix(m1, m2, mat1,
-			n1, n2, mat2):
+def multiplyMatrix(m1, m2, mat1,n1, n2, mat2):
 
 	res = [[0 for x in range(n2)]
 			for y in range (m1)]
@@ -69,11 +68,85 @@ def multiplyMatrix(m1, m2, mat1,
     
 
 
-mat1 = [[2, 4], [3, 4]]
-mat2 = [[1, 2], [1, 3]]
-m1, m2, n1, n2 = 2, 2, 2, 2
+# mat1 = [[2, 4], [3, 4]]
+# mat2 = [[1, 2], [1, 3]]
+# m1, m2, n1, n2 = 2, 2, 2, 2
 
-multiplyMatrix(m1, m2, mat1,n1, n2, mat2)
+# multiplyMatrix(m1, m2, mat1,n1, n2, mat2)
+##################################################################
+
+# Python3 program to rotate a matrix by 90 degrees
+N = 4
+
+# An Inplace function to rotate
+# N x N matrix by 90 degrees in
+# anti-clockwise direction
+def rotateMatrix(mat):
+	
+	# Consider all squares one by one
+	for x in range(0, int(N / 2)):
+		
+		# Consider elements in group
+		# of 4 in current square
+		for y in range(x, N-x-1):
+			
+			# store current cell in temp variable
+			temp = mat[x][y]
+
+			# move values from right to top
+			mat[x][y] = mat[y][N-1-x]
+
+			# move values from bottom to right
+			mat[y][N-1-x] = mat[N-1-x][N-1-y]
+
+			# move values from left to bottom
+			mat[N-1-x][N-1-y] = mat[N-1-y][x]
+
+			# assign temp to left
+			mat[N-1-y][x] = temp
+
+
+# Function to print the matrix
+def displayMatrix( mat ):
+	
+	for i in range(0, N):
+		
+		for j in range(0, N):
+			
+			print (mat[i][j], end = ' ')
+		print ("")
+	
+	
+
+
+# Driver Code
+mat = [[0 for x in range(N)] for y in range(N)]
+
+# Test case 1
+mat = [ [1, 2, 3, 4 ],
+		[5, 6, 7, 8 ],
+		[9, 10, 11, 12 ],
+		[13, 14, 15, 16 ] ]
+		
+'''
+# Test case 2
+mat = [ [1, 2, 3 ],
+		[4, 5, 6 ],
+		[7, 8, 9 ] ]
+
+# Test case 3
+mat = [ [1, 2 ],
+		[4, 5 ] ]
+		
+'''
+
+rotateMatrix(mat)
+
+# Print rotated matrix
+displayMatrix(mat)
+
+
+# This code is contributed by saloni1297
 	
 
 
