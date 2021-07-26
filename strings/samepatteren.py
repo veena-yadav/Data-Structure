950d8bb2c50e20b1e614012d3c6629cf05ff3ccd
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-26 15:07:14 +0530

    5

--- strings/samepatteren.py
+++ strings/samepatteren.py
@@ -100,3 +100,27 @@ def convertRoman(n):
 """Input:
 n = 3
 Output: III"""
+
+#####################################################
+def getMaxOccurringChar(str):
+        ASCII_SIZE = 256
+    
+        count = [0] * ASCII_SIZE
+     
+        # Utility variables
+        max = -1
+        c = ''
+        
+        for i in str:
+            count[ord(i)] +=1
+     
+        for i in str:
+            if max < count[ord(i)]:
+                max = count[ord(i)]
+                c = i
+     
+        return c
+    
+# Driver program to test the above function
+str = "output"
+# print ("Max occurring character is " + getMaxOccurringChar(str))
