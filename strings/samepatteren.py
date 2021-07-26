c6bbbb24839f864634d3ca05caebea65792b89e1
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-26 11:12:51 +0530

    3

--- strings/samepatteren.py
+++ strings/samepatteren.py
@@ -74,3 +74,29 @@ def isBinary(str):
         return 1
     else :
         return 0
+
+################################################
+
+def convertRoman(n):
+    #Code here
+    val = [1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1 ]
+    syb = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"]
+    roman_num = ''
+    i = 0
+    while  n > 0:
+        for _ in range(n // val[i]):
+            roman_num += syb[i]
+            n -= val[i]
+        i += 1
+    return roman_num
+
+"""Input:
+n = 3
+Output: III"""
