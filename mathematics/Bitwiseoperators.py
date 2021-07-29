e0d911d8900b25395603c0794493e891b97650e1
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-07-29 11:41:28 +0530

    1

--- mathematics/Bitwiseoperators.py
+++ mathematics/Bitwiseoperators.py
@@ -13,3 +13,27 @@ def isPowerOfTwo(n):
 
 # Driver code
 # print(isPowerOfTwo(9))
+############################################
+# Given a number N, find the most significant set bit in the given number.
+"""Input : N = 10
+Output : 8
+Binary representation of 10 is 1010
+The most significant bit corresponds
+to decimal number 8.
+
+Input : N = 18
+Output : 16"""
+def setBitNumber(n):
+    if (n == 0):
+        return 0
+ 
+    msb = 0
+    n = int(n / 2)
+ 
+    while (n > 0):
+        n = int(n / 2)
+        msb += 1
+ 
+    return (1 << msb)
+
+print(setBitNumber(18))
