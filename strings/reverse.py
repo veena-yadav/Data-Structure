0bed03bc9cce5da721170755c5d8b4b70e8cd4fa
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-08-06 12:52:25 +0530

    4

--- strings/reverse.py
+++ strings/reverse.py
@@ -84,3 +84,17 @@ def isAnagram(s,t):
 #         return t_size == s_size == 0
 
 ##############################################################
+# Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+def isPalindrome(s):
+    import re
+    s = re.sub('[^a-zA-Z0-9]', '', s).lower()
+    p1 = 0
+    p2 = len(s) - 1
+    while p1 < p2:
+        if s[p1] != s[p2]: return False
+        p1 += 1
+        p2 -= 1
+    return True
+
+# s = "A man, a plan, a canal: Panama"
+# print(isPalindrome(s))
