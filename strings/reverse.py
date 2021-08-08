8afc3da55b6fc1e3bd9af4f9897d9f575c5a15d2
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-08-08 15:57:35 +0530

    1

--- strings/reverse.py
+++ strings/reverse.py
@@ -113,3 +113,20 @@ def strStr(self, haystack: str, needle: str) -> int:
         return -1
     elif needle in haystack:
         return haystack.find(needle)
+    
+    
+######################################################################
+"""Input: strs = ["flower","flow","flight"]
+Output: "fl""""
+def longestCommonPrefix(strs: List[str]) -> str:
+    prefix=""
+    if len(strs)==0: return prefix
+
+    for i in range(len(min(strs))):
+        c=strs[0][i]
+        if all(a[i]==c for a in strs):
+            prefix+=c
+        else:
+            break
+    return prefix
+        
